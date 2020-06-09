9c52d1bfff4915cfd8de9806b35497c6b5d1c1c2
hoang <vinhyenvodoi98@gmail.com>
2020-06-09 22:57:57 +0700

    feat : new way control servo

--- sanken.py
+++ sanken.py
@@ -1,6 +1,3 @@
 from servo import *
-import time
 
-runServo(17, 12.5)
-time.sleep(5)
-runServo(17, 5)
+runServo(17)
--- servo.py
+++ servo.py
@@ -2,15 +2,29 @@ import RPi.GPIO as GPIO
 import time
 
 
-def runServo(PIN, rotation):
+def runServo(PIN):
+
     servoPIN = PIN
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(servoPIN, GPIO.OUT)
 
     p = GPIO.PWM(servoPIN, 50)  # GPIO 17 for PWM with 50Hz
-    p.start(5)  # Initialization
+    p.start(0)  # Initialization
     try:
-        p.ChangeDutyCycle(rotation)
+        p.ChangeDutyCycle(0)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(6)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.15)
+        p.ChangeDutyCycle(0)
+        time.sleep(0.15)
 
     except KeyboardInterrupt:
         p.stop()
